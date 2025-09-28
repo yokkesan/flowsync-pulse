de8502af1c30edb1a11dc575e9897bcf7482b4b8
yokkesan <lc.yteshima@gmail.com>
2025-09-28 11:48:20 +0900

    Laravel + Vue + Docker 環境構築

---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel + Vue</title>
+</head>
+<body>
+    <!-- Vue がマウントされる場所 -->
+    <div id="app"></div>
+
+    <!-- Vite がビルドした Vue を読み込み -->
+    @vite('resources/js/app.js')
+</body>
+</html>
