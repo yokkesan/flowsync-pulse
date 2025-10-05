55a09e4ff9c3aa8696149ae2a0abf14f08820567
yokkesan <lc.yteshima@gmail.com>
2025-10-05 18:35:03 +0900

    ログイントップ〜ログインフォーム作成

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,5 +1,22 @@
 import './bootstrap';
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import '../css/app.css';
+import '../css/mainOffice.scss';
+import '../css/sideDrawer.scss';
+import '../css/loginTop.scss'; // ← LoginTop用（存在する場合）
+import '../css/loginForm.scss'
 
-createApp(App).mount('#app');
+createInertiaApp({
+    resolve: (name) =>
+        resolvePageComponent(
+            `./views/${name}.vue`,
+            import.meta.glob('./views/**/*.vue')
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
---
+++ src/resources/js/views/login/LoginTop.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login-top">
+        <h1 class="title">FlowSync Pulse</h1>
+
+        <div class="btn-area">
+            <button @click="goLogin" class="btn btn-login">ログイン</button>
+            <button @click="goRegister" class="btn btn-register">新規登録</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { router as inertiaRouter } from '@inertiajs/vue3'
+
+const goLogin = () => inertiaRouter.visit('/login')
+const goRegister = () => inertiaRouter.visit('/register/step1')
+</script>
+
+<!-- スタイルは別ファイルで管理 -->
